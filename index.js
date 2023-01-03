fe53e958dbeee18787c9016ae82daf99533f9946
Shafin chowdhury <ShafinChowdhury2001@gmail.com>
2023-01-03 20:23:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function deleteSomething(){
+    let value = confirm("Do you want to delete?");
+    if(value){
+    console.log("deleted");}
+
+    else{
+        console.log("let's not delete");
+    }
+
+}
+
+deleteSomething();
